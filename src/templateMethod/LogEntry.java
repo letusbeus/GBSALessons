4daac9dc64be41efc0094db8ec64d17d54a5524e
letusbeus <letusbeus@mail.ru>
2024-08-24 16:50:47 +0300

    Template method added.

---
+++ src/templateMethod/LogEntry.java
@@ -0,0 +1,17 @@
+package templateMethod;
+
+/**
+ * Запись лога
+ */
+
+class LogEntry {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public LogEntry(String text) {
+        this.text = text;
+    }
+}
